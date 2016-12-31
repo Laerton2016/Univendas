34bd6ef5fca257e48f43f9b0edfc50e9f8b8c256
Laerton2016 <laerton240311@gmail.com>
2016-12-31 09:39:24 -0300

    Adicionando as classes de Modelo para os bancos de dados SGBR Laerton e SGBR SIL, Usando Entity Framework e FireBird

---
+++ Univendas/Univendas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Univendas.Model;
+namespace Univendas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Context_S context = new Context_S()) 
+            {
+                var query = from c in context.TESTOQUE select c;
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.PRODUTO);
+                }
+            }
+            Console.Read();
+        }
+        
+    }
+}
